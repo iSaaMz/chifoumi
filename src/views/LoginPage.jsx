638a83af1d5e1fa6c1c2a748bc71f00a1663a0a0
MoussaST <moussa21sr@gmail.com>
2025-01-21 14:16:48 +0100

    Ajout de Tailwind au projet

--- src/views/LoginPage.jsx
+++ src/views/LoginPage.jsx
@@ -2,7 +2,7 @@ import LoginForm  from '../components/auth/LoginForm';
 
 export default function LoginPage() {
     return (
-        <div>
+        <div className='flex justify-center items-center h-screen'>
             <LoginForm />
         </div>
     );
--- src/views/RegisterPage.jsx
+++ src/views/RegisterPage.jsx
@@ -2,7 +2,7 @@ import RegisterForm from '../components/auth/RegisterForm'
 
 export default function RegisterPage() {
   return (
-    <div>
+    <div className='flex items-center justify-center h-screen'>
       <RegisterForm />
     </div>
   )
